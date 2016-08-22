33aa43f812257e813c5fd9916050083bec939ef1
Stefan Mandel <Stefan.Mandel@gmail.com>
2016-08-22 08:17:48 +0200

    new ReaderCharProvider

--- src/main/java/com/almondtools/stringsandchars/io/ReaderCharProvider.java
+++ src/main/java/com/almondtools/stringsandchars/io/ReaderCharProvider.java
@@ -2,72 +2,124 @@ package com.almondtools.stringsandchars.io;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.nio.CharBuffer;
 
 public class ReaderCharProvider implements CharProvider {
 
 	private static final long NO_MARK = Long.MIN_VALUE;
 
 	private Reader input;
-	private int chunk;
-	private int capacity;
-	private CharBuffer buffer;
+	private int bufferSize;
+	private int bufferNumber;
+	private char[][] buffers;
+	private int currentIndex;
+	private int currentPos;
+	private int topIndex;
+	private int topPos;
 	private long absolutePos;
 	private long mark;
-	private int pos;
 
-	private int state;
-
-	public ReaderCharProvider(Reader input, long start, int buffer, int reverseBuffers) {
+	public ReaderCharProvider(Reader input, long start, int chunk, int lookaroundBuffers) {
 		this.input = input;
-		this.chunk = buffer;
-		this.capacity = buffer * (reverseBuffers + 1);
-		this.buffer = CharBuffer.allocate(capacity);
+		this.bufferSize = chunk;
+		this.bufferNumber = lookaroundBuffers + 1;
+		this.buffers = new char[bufferNumber][bufferSize];
+		this.currentIndex = -1;
+		this.currentPos = 0;
+		this.topIndex = -1;
+		this.topPos = 0;
 		this.mark = NO_MARK;
 		init(start);
 	}
 
 	public final void init(long start) {
-		long shift = ((start / chunk) + 1) * chunk;
-		long skip = 0;
-		while (shift > capacity) {
-			shift -= chunk;
-			skip += chunk;
-		}
-		if (skip > 0) {
-			skip(skip);
-		}
-		read();
-		pos = (int) (start - skip);
+		int buffersToRead = (int) (start / (long) bufferSize) + 1;
+		read(buffersToRead);
+		currentIndex = topIndex;
+		currentPos = (int) (start % bufferSize);
 		absolutePos = start;
 	}
 
-	public void read() {
-		buffer.position(0);
-		buffer.limit(capacity);
+	private void read(int buffersToRead) {
+		if (buffersToRead <= 0) {
+			return;
+		}
 		try {
-			state = input.read(buffer);
+			int buffersAlreadyRead = topIndex - currentIndex;
+			for (int i = 0; i < buffersAlreadyRead; i++) {
+				buffersToRead--;
+			}
+
+			int buffersToSkip = buffersToRead - bufferNumber;
+			if (buffersToSkip > 0) {
+				for (int i = 0; i < buffersToSkip; i++) {
+					topPos = input.read(buffers[0]);
+					buffersToRead--;
+				}
+				topIndex = -1;
+				currentIndex = -1;
+			}
+
+			int freeBuffers = bufferNumber - currentIndex - 1;
+			int buffersToShift = buffersToRead - freeBuffers;
+
+			if (buffersToShift > 0) {
+				int lastIndexToReplace = bufferNumber - buffersToShift;
+				for (int i = 0; i < lastIndexToReplace; i++) {
+					char[] temp = buffers[i];
+					buffers[i] = buffers[i + buffersToShift];
+					buffers[i + buffersToShift] = temp;
+				}
+				topIndex -= buffersToShift;
+			}
+
+			while (buffersToRead > 0) {
+				topIndex++;
+				topPos = input.read(buffers[topIndex]);
+				buffersToRead--;
+				if (topPos < bufferSize) {
+					break;
+				}
+			}
+			if (buffersToRead > 0) {
+				throw new OutOfBufferException();
+			}
 		} catch (IOException e) {
 			throw new IORuntimeException(e);
 		}
-		buffer.flip();
 	}
 
-	public void shift(int shift) {
-		buffer.position(shift);
-		buffer.compact();
-		try {
-			state = input.read(buffer);
-		} catch (IOException e) {
-			throw new IORuntimeException(e);
+	private void readAhead(int buffersToRead) {
+		if (buffersToRead <= 0) {
+			return;
 		}
-		buffer.flip();
-		pos -= shift;
-	}
-
-	public void skip(long skip) {
 		try {
-			input.skip(skip);
+			int freeBuffers = bufferNumber - topIndex - 1;
+			int buffersToShift = buffersToRead - freeBuffers;
+
+			if (buffersToShift > currentIndex) {
+				throw new OutOfBufferException();
+			} else if (buffersToShift > 0) {
+				int lastIndexToReplace = bufferNumber - buffersToShift;
+				for (int i = 0; i < lastIndexToReplace; i++) {
+					char[] temp = buffers[i];
+					buffers[i] = buffers[i + buffersToShift];
+					buffers[i + buffersToShift] = temp;
+				}
+				currentIndex -= buffersToShift;
+				topIndex -= buffersToShift;
+			}
+
+			while (buffersToRead > 0) {
+				topIndex++;
+				topPos = input.read(buffers[topIndex]);
+				buffersToRead--;
+				if (topPos < bufferSize) {
+					break;
+				}
+			}
+			if (buffersToRead > 0) {
+				throw new OutOfBufferException();
+			}
 		} catch (IOException e) {
 			throw new IORuntimeException(e);
 		}
@@ -75,43 +127,78 @@ public class ReaderCharProvider implements CharProvider {
 
 	@Override
 	public char next() {
-		if (pos >= buffer.limit()) {
-			shift(chunk);
+		int expectedIndex = currentIndex;
+		while (currentPos >= bufferSize) {
+			currentPos -= bufferSize;
+			expectedIndex++;
 		}
-		char c = buffer.charAt(pos);
-		pos++;
+		if (expectedIndex <= topIndex) {
+			currentIndex = expectedIndex;
+		} else {
+			read(expectedIndex - currentIndex);
+			currentIndex = topIndex;
+		}
+		char c = buffers[currentIndex][currentPos];
+		currentPos++;
 		absolutePos++;
 		return c;
 	}
 
 	@Override
 	public char lookahead() {
-		return buffer.charAt(pos);
+		return lookahead(0);
 	}
 
 	@Override
-	public char lookahead(int i) {
-		return buffer.charAt(pos + i);
+	public char lookahead(int pos) {
+		int expectedPos = currentPos + pos;
+		int expectedIndex = currentIndex;
+		while (expectedPos >= bufferSize) {
+			expectedPos -= bufferSize;
+			expectedIndex++;
+		}
+		if (expectedIndex > topIndex) {
+			readAhead(expectedIndex - topIndex);
+		}
+		char c = buffers[expectedIndex][expectedPos];
+		return c;
 	}
 
 	@Override
 	public char prev() {
-		pos--;
+		int expectedIndex = currentIndex;
+		currentPos--;
 		absolutePos--;
-		if (pos < 0) {
+		while (currentPos < 0 && expectedIndex > -1) {
+			currentPos += bufferSize;
+			expectedIndex--;
+		}
+		if (expectedIndex < 0) {
 			throw new OutOfBufferException();
 		}
-		return buffer.charAt(pos);
+		currentIndex = expectedIndex;
+		char c = buffers[currentIndex][currentPos];
+		return c;
 	}
 
 	@Override
 	public char lookbehind() {
-		return buffer.charAt(pos - 1);
+		return lookbehind(0);
 	}
 
 	@Override
-	public char lookbehind(int i) {
-		return buffer.charAt(pos - 1 - i);
+	public char lookbehind(int pos) {
+		int expectedPos = currentPos - 1 - pos;
+		int expectedIndex = currentIndex;
+		while (expectedPos < 0) {
+			expectedPos += bufferSize;
+			expectedIndex--;
+		}
+		if (expectedIndex < 0) {
+			throw new OutOfBufferException();
+		}
+		char c = buffers[expectedIndex][expectedPos];
+		return c;
 	}
 
 	@Override
@@ -120,29 +207,30 @@ public class ReaderCharProvider implements CharProvider {
 	}
 
 	@Override
-	public void move(long i) {
-		long rel = i - absolutePos;
-		if (pos + rel < 0) {
-			int rev = pos + 1;
-			pos = -1;
-			absolutePos -= rev;
-			throw new OutOfBufferException();
+	public void move(long pos) {
+		long relativePos = pos - absolutePos;
+		long expectedPos = (long) currentPos + relativePos;
+
+		if (relativePos > 0) {
+			int buffersToRead = (int) (expectedPos / (long) bufferSize);
+			read(buffersToRead);
+			currentIndex = topIndex;
+			currentPos = (int) (expectedPos % bufferSize);
+			absolutePos = pos;
+		} else {
+			int expectedIndex = currentIndex;
+			while (expectedPos < 0 && expectedIndex > -1) {
+				expectedPos += bufferSize;
+				expectedIndex--;
+			}
+			if (expectedIndex < 0) {
+				throw new OutOfBufferException();
+			} else {
+				currentIndex = expectedIndex;
+				currentPos = (int) expectedPos;
+				absolutePos = pos;
+			}
 		}
-		int shift = 0;
-		while (pos - shift + rel > buffer.limit()) {
-			shift += chunk;
-		}
-		int skip = 0;
-		while (shift > capacity) {
-			shift -= chunk;
-			skip += chunk;
-		}
-		if (skip > 0) {
-			skip(skip);
-		}
-		shift(shift);
-		pos += rel;
-		absolutePos += rel;
 	}
 
 	@Override
@@ -153,8 +241,8 @@ public class ReaderCharProvider implements CharProvider {
 	@Override
 	public void finish() {
 		try {
-			pos = buffer.limit();
-			state = -1;
+			topPos = -1;
+			currentIndex = topIndex;
 			input.close();
 		} catch (IOException e) {
 			throw new IORuntimeException(e);
@@ -163,92 +251,128 @@ public class ReaderCharProvider implements CharProvider {
 
 	@Override
 	public boolean finished() {
-		if (buffer.limit() - pos > 0) {
-			return false;
-		} else if (buffer.limit() - pos == 0 && state == -1) {
+		if (topPos == -1 && currentIndex == topIndex) {
+			return true;
+		} else if (topPos == -1 && currentIndex == topIndex - 1 && currentPos == bufferSize) {
 			return true;
 		}
-		shift(chunk);
-		return buffer.limit() - pos <= 0 && state == -1;
+		return false;
 	}
 
 	@Override
 	public boolean finished(int i) {
-		if (buffer.limit() - pos > i) {
-			return false;
-		} else if (buffer.limit() - pos <= i && state == -1) {
+		int expectedPos = currentPos + i;
+		int expectedIndex = currentIndex;
+		while (expectedPos > bufferSize) {
+			expectedPos -= bufferSize;
+			expectedIndex++;
+		}
+		if (expectedIndex > topIndex) {
+			readAhead(expectedIndex - topIndex);
+		}
+		if (expectedIndex == topIndex && expectedPos == topPos && topPos == bufferSize) {
+			readAhead(1);
+		} 
+		
+		if (topPos == -1 && expectedIndex >= topIndex) {
+			return true;
+		} else if (topPos == -1 && expectedIndex == topIndex - 1 && expectedPos == bufferSize) {
 			return true;
 		}
-		if ((pos % chunk) + i >= capacity) {
-			throw new OutOfBufferException();
-		}
-		int shift = 0;
-		while (pos - shift + i >= capacity) {
-			shift += chunk;
-		}
-		if (shift > 0 || buffer.limit() < capacity) {
-			shift(shift);
-		}
-		return buffer.limit() - pos <= i && state == -1;
+		return false;
 	}
 
 	@Override
-	public char at(long i) {
-		long rel = i - absolutePos;
-		if (pos + rel < 0) {
-			int rev = pos + 1;
-			pos = -1;
-			absolutePos -= rev;
+	public char at(long pos) {
+		long relativePos = pos - absolutePos;
+		long expectedPos = (long) currentPos + relativePos;
+		int expectedIndex = currentIndex;
+		while (expectedPos < 0) {
+			expectedPos += bufferSize;
+			expectedIndex--;
+		}
+		while (expectedPos >= bufferSize) {
+			expectedPos -= bufferSize;
+			expectedIndex++;
+		}
+
+		if (expectedIndex < 0) {
 			throw new OutOfBufferException();
 		}
-		if ((pos % chunk) + rel >= capacity) {
-			throw new OutOfBufferException();
+		if (expectedIndex > topIndex) {
+			int oldIndex = currentIndex;
+			readAhead(expectedIndex - topIndex);
+			int indexShift = oldIndex - currentIndex;
+			expectedIndex -= indexShift;
 		}
-		int shift = 0;
-		while (pos - shift + rel >= capacity) {
-			shift += chunk;
-		}
-		if (shift > 0 || buffer.limit() < capacity) {
-			shift(shift);
-		}
-		return buffer.charAt((int) (pos + rel));
+
+		return buffers[expectedIndex][(int) expectedPos];
 	}
 
 	@Override
 	public char[] between(long start, long end) {
-		if (end - start + (start % chunk) > capacity) {
+		long len = end - start;
+		if (len / bufferSize > bufferNumber) {
 			throw new OutOfBufferException();
 		}
-		//capacity is sufficient
 
-		long startRel = start - absolutePos;
-		long endRel = end - absolutePos;
-		if (pos + startRel < 0) {
-			int rev = pos + 1;
-			pos = -1;
-			absolutePos -= rev;
+		long relativeStart = start - absolutePos;
+		long expectedStartPos = (long) currentPos + relativeStart;
+		int expectedStartIndex = currentIndex;
+		while (expectedStartPos < 0) {
+			expectedStartPos += bufferSize;
+			expectedStartIndex--;
+		}
+		while (expectedStartPos >= bufferSize) {
+			expectedStartPos -= bufferSize;
+			expectedStartIndex++;
+		}
+
+		long relativeEnd = end - absolutePos;
+		long expectedEndPos = (long) currentPos + relativeEnd;
+		int expectedEndIndex = currentIndex;
+		while (expectedEndPos <= 0) {
+			expectedEndPos += bufferSize;
+			expectedEndIndex--;
+		}
+		while (expectedEndPos > bufferSize) {
+			expectedEndPos -= bufferSize;
+			expectedEndIndex++;
+		}
+
+		if (expectedStartIndex < 0 || expectedEndIndex < 0) {
+			throw new OutOfBufferException();
+		} else if (expectedEndIndex - expectedStartIndex >= bufferNumber) {
 			throw new OutOfBufferException();
 		}
-		//startRel is reachable
-
-		int shift = 0;
-		while (pos - shift + endRel >= buffer.limit()) {
-			shift += chunk;
+		if (expectedEndIndex > topIndex) {
+			int oldIndex = currentIndex;
+			readAhead(expectedEndIndex - topIndex);
+			int indexShift = oldIndex - currentIndex;
+			expectedStartIndex -= indexShift;
+			expectedEndIndex -= indexShift;
 		}
-		if (shift > pos) {
-			throw new OutOfBufferException();
-		}
-		if (shift > 0 || buffer.limit() < capacity) {
-			shift(shift);
-		}
-		//endRel is reachable
 
-		int startSeq = (int) (pos + startRel);
-		int endSeq = (int) (pos + endRel);
+		int betweenLen = (int) len;
+		char[] between = new char[betweenLen];
 
-		char[] cs = new char[endSeq - startSeq];
-		buffer.subSequence(startSeq, endSeq).get(cs);
-		return cs;
+		if (expectedStartIndex == expectedEndIndex) {
+			System.arraycopy(buffers[expectedStartIndex], (int) expectedStartPos, between, 0, (int) (expectedEndPos - expectedStartPos));
+		} else {
+			int to = 0;
+
+			System.arraycopy(buffers[expectedStartIndex], (int) expectedStartPos, between, to, (int) (bufferSize - expectedStartPos));
+			to += bufferSize - expectedStartPos;
+
+			for (int i = expectedStartIndex + 1; i < expectedEndIndex; i++) {
+				System.arraycopy(buffers[i], 0, between, to, bufferSize);
+				to += bufferSize;
+			}
+
+			System.arraycopy(buffers[expectedEndIndex], 0, between, to, (int) expectedEndPos);
+			to += expectedEndPos;
+		}
+		return between;
 	}
 
 	@Override
@@ -270,7 +394,17 @@ public class ReaderCharProvider implements CharProvider {
 
 	@Override
 	public String toString() {
-		return "..." + buffer.subSequence(0, pos) + '|' + buffer.subSequence(pos, buffer.limit());
+		StringBuilder buffer = new StringBuilder("...");
+		if (currentIndex > 0) {
+			buffer.append(buffers[currentIndex - 1]);
+		}
+		buffer.append(buffers[currentIndex], 0, currentPos);
+		buffer.append('|');
+		if (topIndex > currentIndex) {
+			buffer.append(buffers[currentIndex], currentPos, bufferSize - currentPos);
+		} else if (topPos > currentPos) {
+			buffer.append(buffers[currentIndex], currentPos, topPos - currentPos);
+		}
+		return buffer.toString();
 	}
-
 }
